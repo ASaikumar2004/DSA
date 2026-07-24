8b6b2585da38aa09b37e7e73315fe93ccbd7a599
ASaikumar2004 <ampolusaikumar2004@gmail.com>
2026-07-24 14:59:21 +0530

    added a file oin arrays

---
+++ Arrays/Employee.java
@@ -0,0 +1,27 @@
+package Arrays;
+
+public class Employee {
+
+	int emipd;
+	String empname;
+	
+	public Employee(int empid,String empname) {
+		this.emipd=empid;
+		this.empname=empname;
+	}
+	@Override
+	public String toString() {
+		return "Employee [empid=" +emipd+",empname="+empname +"]";
+	}
+	
+	public static void main(String[] args) {
+		Employee e=new Employee(23, "Sai");
+		Employee e1=new Employee(2004, "Saikumar");
+		Employee e2=new Employee(23, "Sai sdfgh");
+		
+		Employee emp[]= {e,e1,e2};
+		for(Employee st:emp) {
+			System.out.println(st);
+		}
+	}
+}
