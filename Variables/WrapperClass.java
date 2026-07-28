e1fa8d37cec6a64ce923ff99f25d523474ef2021
ASaikumar2004 <ampolusaikumar2004@gmail.com>
2026-07-28 21:24:06 +0530

    modified

--- Variables/WrapperClass.java
+++ Variables/WrapperClass.java
@@ -1,6 +1,7 @@
 package Variables;
 
 public class WrapperClass {
+	
 	public static void main(String[] args) {
 		double salary=Double.parseDouble(args[0]);
 		double annual=Double.parseDouble(args[1]);
