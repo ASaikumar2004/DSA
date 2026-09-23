2fae2c1ebc5fb60c98bcc9c05e4c830a0cf043e7
ASaikumar2004 <ampolusaikumar2004@gmail.com>
2026-09-23 14:39:25 +0530

    added new concept like bubble sort

---
+++ SortingTechniquies/Bubblesort.java
@@ -0,0 +1,32 @@
+package SortingTechniquies;
+
+public class Bubblesort {
+
+	public static void main(String[] args) {
+		
+		int arr[]= {1,2,3,4,5};
+		boolean swapped=false;
+		for(int i=0;i<arr.length-1;i++) {
+			for(int j=0;j<arr.length-i-1;j++) {
+				if(arr[j]>arr[j+1]) {
+					int temp=arr[j];
+					arr[j]=arr[j+1];
+					arr[j+1]=temp;
+					swapped=true;
+				}
+			}
+			
+			
+			
+			if(!swapped) {
+				System.out.println("already sorted");
+				break;
+			}
+		}
+		
+		
+		for(int i:arr) {
+			System.out.print(i+" ");
+		}
+	}
+}
